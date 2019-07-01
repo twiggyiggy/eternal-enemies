dabde7297e1cfc31f558385299c6878cff7e7454
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-07-01 21:45:27 +0200

    add full functionality to game

--- Enemy.js
+++ Enemy.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function Enemy(canvas, randomY) {
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext('2d');
+  this.x = this.canvas.width;
+  this.y = randomY;
+  this.velocity = 3;
+  this.direction = -1;
+  this.color = 'red';
+  this.width = 10;
+  this.height = 10;
+}
+
+Enemy.prototype.move = function() {
+  this.x = this.x + this.direction * this.velocity;
+}
+
+Enemy.prototype.draw = function() {
+  this.ctx.fillStyle = this.color;
+  this.ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-'use strict';
+"use strict";
+
+function main() {
+  var mainElement = document.querySelector('#site-main');
+
+  function buildDom(html) {
+    mainElement.innerHTML = html;
+    return mainElement;
+  }
+
+  function createSplashScreen() {
+    var splashScreen = buildDom(`
+      <section>
+        <h1>Eternal Enemies</h1>
+        <button>Start</button>
+      </section>
+    `);
+    var startButton = splashScreen.querySelector('button');
+    startButton.addEventListener("click", createGameScreen);
+  }
+
+  function createGameScreen() {
+    var gameOverScreen = buildDom(`
+      <section>
+        <canvas width="500" height="500"></canvas>
+      </section>
+    `);
+
+    var canvas = document.querySelector('canvas');
+    var gameInstance = new Game(canvas);
+
+    gameInstance.gameOverCallback(createGameOverScreen);
+    
+    gameInstance.startGame();
+    document.addEventListener('keydown', function(event) {
+      if(event.key === 'ArrowDown') {
+        gameInstance.player.setDirection(1);
+      } else if(event.key === 'ArrowUp') {
+        gameInstance.player.setDirection(-1);
+      }
+    })
+  };
+
+
+  function createGameOverScreen() {
+    var gameOverScreen = buildDom(`
+      <section>
+        <h1>Game Over</h1>
+        <button>Restart</button>
+      </section>
+    `);
+    var restartButton = gameOverScreen.querySelector('button');
+    restartButton.addEventListener("click", createSplashScreen);
+  }
+
+  createSplashScreen();
+}
+
+window.addEventListener("load", main);
